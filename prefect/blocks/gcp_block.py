818d11cbe6849df6585f1d703d9582691a83d30b
Brett <brettdavies@users.noreply.github.com>
2023-08-10 17:28:44 -0700

    Add support for project_id variable in Prefect block configuration
    Add support for project_id variable in Prefect block configuration

--- prefect/blocks/gcp_block.py
+++ prefect/blocks/gcp_block.py
@@ -1,19 +1,21 @@
+import os
 from prefect_gcp import GcpCredentials
 from prefect_gcp.cloud_storage import GcsBucket
 
+# Access the environment variable
+project_id = os.getenv('project_id')
 
-# replace this PLACEHOLDER dict with your the service account info from the JSON file
 service_account_info = {
   "type": "service_account",
-  "project_id": "[redacted]",
+  "project_id": f"{project_id}",
   "private_key_id": "[redacted]",
   "private_key": "-----BEGIN PRIVATE KEY-----\n[redacted]\n-----END PRIVATE KEY-----\n",
-  "client_email": "tf-serviceaccount@[redacted].iam.gserviceaccount.com",
+  "client_email": f"tf-serviceaccount@{project_id}.iam.gserviceaccount.com",
   "client_id": "[redacted]",
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
   "token_uri": "https://oauth2.googleapis.com/token",
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/tf-serviceaccount@[redacted].iam.gserviceaccount.com",
+  "client_x509_cert_url": f"https://www.googleapis.com/robot/v1/metadata/x509/tf-serviceaccount@{project_id}.iam.gserviceaccount.com",
   "universe_domain": "googleapis.com"
 }
 
@@ -23,7 +25,7 @@ block = GcpCredentials(
 block.save("gcp-cred",overwrite=True)
 
 block2 = GcsBucket(
-  bucket="dl_raw_decap_cli",
+  bucket=f"dl_raw_{project_id}",
   bucket_folder="",
   gcp_credentials=GcpCredentials.load("gcp-cred")
 )
